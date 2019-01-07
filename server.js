b9699097f2a0db68c1147f82a6175da77925bf82
leomendoza123 <leomendoza123@gmail.com>
2019-01-06 20:26:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin'],
-    removeHeaders: ['origin'],
+    removeHeaders: [
+    'origin',     
+    // Strip Heroku-specific headers
+    'x-heroku-queue-wait-time',
+    'x-heroku-queue-depth',
+    'x-heroku-dynos-in-use',
+    'x-request-start',],
   })
   .listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port)
